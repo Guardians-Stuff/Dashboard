c81e4f758407f890882fa386065a356fac95a1e1
Nyxa <116373322+NyxaYu@users.noreply.github.com>
2023-02-07 01:50:26 +0000

    Add server

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config({ path: `${__dirname}/.env.local` });
+
+const Next = require('next');
+const https = require('https');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const dev = process.env.NODE_ENV === 'development';
+const protocol = process.env.NEXT_PROTOCOL;
+const hostname = process.env.NEXT_HOSTNAME;
+const port = process.env.NEXT_PORT;
+
+const app = Next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    let server;
+    let options = {};
+
+    if(protocol == 'http') {
+        server = http;
+    }else{
+        server = https;
+        options = { key: fs.readFileSync(`${__dirname}/certificates/privkey.pem`), cert: fs.readFileSync(`${__dirname}/certificates/fullchain.pem`) };
+    }
+
+    server.createServer(options, async (req, res) => {
+        try {
+            const parsedUrl = url.parse(req.url, true);
+            await handle(req, res, parsedUrl);
+        } catch (err) {
+            console.error('Error occurred handling', req.url, err);
+
+            res.statusCode = 500;
+            res.end('internal server error');
+        }
+    }).listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on ${protocol}://${hostname}:${port}`);
+    });
+});
