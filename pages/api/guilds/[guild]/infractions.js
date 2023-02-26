0f72eee48e313cbb4ed071fc5da70adec691a3cf
Nyxa <116373322+NyxaYu@users.noreply.github.com>
2023-02-26 03:17:46 +0000

    Add pagination support

--- pages/api/guilds/[guild]/infractions.js
+++ pages/api/guilds/[guild]/infractions.js
@@ -10,24 +10,49 @@ import Infractions from '@/schemas/Infractions';
  * @param {NextApiResponse} res
  */
 export default async function handler(req, res) {
+    let pagination = req.query.pagination;
+    if(isNaN(pagination) || pagination < 1) pagination = 1;
+
     /** @type {import('next-auth/providers/discord').DiscordProfile} */ const session = await getServerSession(req, res, authOptions);
-    if(!session && req.headers.authorization != `Bearer ${process.env.DISCORD_CLIENT_TOKEN}`) return res.status(403).json({ error: true, message: 'You must be logged in to do this', infractions: [] });
+    if(!session && req.headers.authorization != `Bearer ${process.env.DISCORD_CLIENT_TOKEN}`) return res.status(403).json({
+        error: true,
+        message: 'You must be logged in to do this',
+        infractions: [],
+        pagination: { page: pagination, totalPages: 0 }
+    });
 
     await dbConnect();
 
-    Infractions.find({ guild: req.query.guild }).sort({ _id: -1 }).then(async (/** @type {Array<import('@/schemas/Infractions').Infraction>} */ results) => {
-        /** @type {Array<import('@/schemas/Infractions').Infraction>} */ const infractions = results.map(result => {
-            /** @type {import('@/schemas/Infractions').Infraction} */ const infraction = result.toObject();
-            infraction._id = result._id.toString();
+    const ids = await Infractions.find({}, '_id').sort({ _id: -1 }).lean();
+    const pages = [];
 
-            return infraction;
+    for(let i = 0; i < ids.length; i += 20){
+        const tempIds = ids.slice(i, i + 20);
+        pages.push(tempIds[0]);
+    }
+
+    Infractions
+        .find({ _id: { $lte: pages[pagination - 1]?._id }, guild: req.query.guild })
+        .sort({ _id: -1 })
+        .limit(20)
+        .lean()
+        .then(async (/** @type {Array<import('@/schemas/Infractions').Infraction>} */ results) => {
+            const infractions = results.map(infraction => {
+                infraction._id = infraction._id.toString();
+
+                return infraction;
+            });
+
+
+            await verifyPermissions(req, res, session, infractions, { page: pagination, totalPages: pages.length });
+        }).catch(() => {
+            res.status(500).json({
+                error: true,
+                message: 'Something went wrong',
+                infractions: [],
+                pagination: { page: pagination, totalPages: pages.length }
+            });
         });
-
-
-        await verifyPermissions(req, res, session, infractions);
-    }).catch(() => {
-        res.status(500).json({ error: true, message: 'Something went wrong', infractions: [] });
-    });
 }
 
 /**
@@ -35,12 +60,23 @@ export default async function handler(req, res) {
  * @param {NextApiResponse} res
  * @param {import('next-auth/providers/discord').DiscordProfile} session
  * @param {Array<import('@/schemas/Infractions').Infraction>} infractions
+ * @param {{ page: Number, totalPages: Number }} pagination
  */
-async function verifyPermissions(req, res, session, infractions){
+async function verifyPermissions(req, res, session, infractions, pagination){
     if(session){
         const auth = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/auth/guilds/${req.query.guild}`, { cache: 'no-cache', headers: { Cookie: req.headers.cookie } });
-        if(!auth.ok) return res.status(401).json({ error: true, message: 'Unauthorized', infractions: [] });
+        if(!auth.ok) return res.status(401).json({
+            error: true,
+            message: 'Unauthorized',
+            infractions: [],
+            pagination: pagination
+        });
     }
 
-    res.status(200).json({ error: false, message: '', infractions: infractions });
+    res.status(200).json({
+        error: false,
+        message: '',
+        infractions: infractions,
+        pagination: pagination
+    });
 }
--- pages/api/guilds/[guild]/tickets.js
+++ pages/api/guilds/[guild]/tickets.js
@@ -10,26 +10,50 @@ import Tickets from '@/schemas/Tickets';
  * @param {NextApiResponse} res
  */
 export default async function handler(req, res) {
+    let pagination = req.query.pagination;
+    if(isNaN(pagination) || pagination < 1) pagination = 1;
+
     /** @type {import('next-auth/providers/discord').DiscordProfile} */ const session = await getServerSession(req, res, authOptions);
-    if(!session && req.headers.authorization != `Bearer ${process.env.DISCORD_CLIENT_TOKEN}`) return res.status(403).json({ error: true, message: 'You must be logged in to do this', tickets: [] });
+    if(!session && req.headers.authorization != `Bearer ${process.env.DISCORD_CLIENT_TOKEN}`) return res.status(403).json({
+        error: true,
+        message: 'You must be logged in to do this',
+        tickets: [],
+        pagination: { page: pagination, totalPages: 0 }
+    });
 
     await dbConnect();
 
-    Tickets.find({ guild: req.query.guild }, 'guild user channel active').sort({ _id: -1 }).then(async (/** @type {Array<import('@/schemas/Tickets').Ticket>} */ results) => {
-        /** @type {Array<import('@/schemas/Tickets').Ticket>} */ const tickets = results.map(result => {
-            /** @type {import('@/schemas/Tickets').Ticket} */ const ticket = result.toObject();
-            ticket._id = result._id.toString();
-            ticket.time = result._id.getTimestamp().valueOf();
-            ticket.messages = [];
+    const ids = await Tickets.find({}, '_id').sort({ _id: -1 }).lean();
+    const pages = [];
 
-            return ticket;
+    for(let i = 0; i < ids.length; i += 20){
+        const tempIds = ids.slice(i, i + 20);
+        pages.push(tempIds[0]);
+    }
+
+    Tickets
+        .find({ _id: { $lte: pages[pagination - 1]?._id }, guild: req.query.guild }, 'guild user channel active')
+        .sort({ _id: -1 })
+        .limit(20)
+        .lean()
+        .then(async (/** @type {Array<import('@/schemas/Tickets').Ticket>} */ results) => {
+            /** @type {Array<import('@/schemas/Tickets').Ticket>} */ const tickets = results.map(ticket => {
+                ticket.time = ticket._id.getTimestamp().valueOf();
+                ticket._id = ticket._id.toString();
+                ticket.messages = [];
+
+                return ticket;
+            });
+
+            await verifyPermissions(req, res, session, tickets, { page: pagination, totalPages: pages.length });
+        }).catch(() => {
+            res.status(500).json({
+                error: true,
+                message: 'Something went wrong',
+                tickets: [],
+                pagination: { page: pagination, totalPages: pages.length }
+            });
         });
-
-
-        await verifyPermissions(req, res, session, tickets);
-    }).catch(() => {
-        res.status(500).json({ error: true, message: 'Something went wrong', tickets: [] });
-    });
 }
 
 /**
@@ -37,12 +61,23 @@ export default async function handler(req, res) {
  * @param {NextApiResponse} res
  * @param {import('next-auth/providers/discord').DiscordProfile} session
  * @param {Array<import('@/schemas/Tickets').Ticket>} tickets
+ * @param {{ page: Number, totalPages: Number }} pagination
  */
-async function verifyPermissions(req, res, session, tickets){
+async function verifyPermissions(req, res, session, tickets, pagination){
     if(session){
         const auth = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/auth/guilds/${req.query.guild}`, { cache: 'no-cache', headers: { Cookie: req.headers.cookie } });
-        if(!auth.ok) return res.status(401).json({ error: true, message: 'Unauthorized', tickets: [] });
+        if(!auth.ok) return res.status(401).json({
+            error: true,
+            message: 'Unauthorized',
+            tickets: [],
+            pagination: pagination
+        });
     }
 
-    res.status(200).json({ error: false, message: '', tickets: tickets });
+    res.status(200).json({
+        error: false,
+        message: '',
+        tickets: tickets,
+        pagination: pagination
+    });
 }
